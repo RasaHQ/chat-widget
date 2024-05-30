6c8db0d7f1ebf87f8f09f61b62beb23ef4befdcb
Sava Vidakovic <vidakovic.sava@gmail.com>
2024-05-30 12:39:14 +0200

    Initial project setup (#1)

---
+++ packages/react/lib/components/stencil-generated/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'web-component';
+
+
+
+export const RasaChatbotWidget = /*@__PURE__*/createReactComponent<JSX.RasaChatbotWidget, HTMLRasaChatbotWidgetElement>('rasa-chatbot-widget');
---
+++ packages/web-component/src/rasa-chatbot-widget/readme.md
@@ -0,0 +1,10 @@
+# rasa-chatbot-widget
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
