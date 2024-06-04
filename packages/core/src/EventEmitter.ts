397418e45969a311f06512049d59ffbaee73d03e
Sava Vidakovic <vidakovic.sava@gmail.com>
2024-06-04 15:42:16 +0200

    Core: Add socket communication, event emitter, and tests (#5)

---
+++ packages/core/src/EventEmitter.ts
@@ -0,0 +1,34 @@
+interface EventMap {
+  connect: () => void;
+  disconnect: () => void;
+  message: (data: unknown) => void;
+}
+
+type EventCallback<T extends keyof EventMap> = EventMap[T];
+
+export class EventEmitter {
+  private events: { [K in keyof EventMap]?: EventCallback<K>[] } = {};
+
+  public on<K extends keyof EventMap>(
+    eventName: K,
+    callback: EventCallback<K>
+  ): void {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName]!.push(callback);
+  }
+
+  protected trigger<K extends keyof EventMap>(
+    eventName: K,
+    ...args: Parameters<EventCallback<K>>
+  ): void {
+    if (this.events[eventName]) {
+      this.events[eventName]!.forEach((callback) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        return callback(...args);
+      });
+    }
+  }
+}
---
+++ packages/core/src/messageParser.ts
@@ -0,0 +1,3 @@
+export const messageParser = (messageFromServer: unknown) => {
+  return messageFromServer;
+};
