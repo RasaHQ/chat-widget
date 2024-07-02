79cb1684dc9c51771b418c6db6da9cafdf0d5f8a
petar-basic <petarbasic1910@gmail.com>
2024-07-02 09:40:36 +0200

    Addressing PR comments

--- packages/web-component/src/components/error-handler/global-error-handler.tsx
+++ packages/web-component/src/components/error-handler/global-error-handler.tsx
@@ -1,6 +1,6 @@
 import { Component, Host, h } from '@stencil/core';
 import { errorMessageService } from '../../store/error-message';
-import { CUSTOM_ERROR_NAME, CustomErrorClass } from '@rasa-widget/core';
+import { CUSTOM_ERROR_NAME, CustomErrorClass, ErrorSeverity } from '@rasa-widget/core';
 
 @Component({
   tag: 'global-error-handler',
@@ -9,11 +9,15 @@ import { CUSTOM_ERROR_NAME, CustomErrorClass } from '@rasa-widget/core';
 export class ErrorHandler {
   connectedCallback() {
     window.onerror = (...[, , , , error]) => {
-      console.log(error instanceof CustomErrorClass);
-      console.log(error);
       if (error instanceof CustomErrorClass && error.name === CUSTOM_ERROR_NAME) {
-        console.log(error.description);
-        errorMessageService.setErrorMessage(error as CustomErrorClass);
+        switch (error.severity) {
+          case ErrorSeverity.Error:
+            errorMessageService.setErrorMessage(error as CustomErrorClass);
+          case ErrorSeverity.LogError:
+            console.error(error.message, error.description);
+          case ErrorSeverity.LogWarning:
+            console.warn(error.message, error.description);
+        }
         return true;
       }
     };
